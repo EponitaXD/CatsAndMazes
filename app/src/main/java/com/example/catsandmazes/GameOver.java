53f4f8b1c939ebfc17a5579cfa61b44f5801e1a9
EponitaXD <mariajosecamargouseche@gmail.com>
2025-04-23 21:50:52 -0400

    Add cat noises

--- app/src/main/java/com/example/catsandmazes/GameOver.java
+++ app/src/main/java/com/example/catsandmazes/GameOver.java
@@ -1,9 +1,15 @@
 package com.example.catsandmazes;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 //import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOver extends Activity {
-
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gameover_lose);
+    }
 }
